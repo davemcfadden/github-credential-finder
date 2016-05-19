f9358855c3ebee53f722f3a32daca81222de1ce5
davemcfadden <david.mcfadden@outlook.com>
2016-05-19 22:53:45 +0100

    adding directory scanner and refactoring config

--- src/main/java/org/credential/finder/api/FinderInializer.java
+++ src/main/java/org/credential/finder/api/FinderInializer.java
@@ -5,48 +5,32 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.log4j.Logger;
-import org.credential.finder.issue.generator.IssueGenerator;
+import org.credential.finder.config.GitConfig;
+import org.credential.finder.repository.scanner.RepositoryScanner;
 import org.eclipse.egit.github.core.Repository;
-import org.eclipse.egit.github.core.client.GitHubClient;
-import org.eclipse.egit.github.core.service.RepositoryService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class FinderInializer{
 	
-	@Value("${github.username}")
-	String username;
-
-	@Value("${github.password}")
-	String password;
+	@Autowired
+	RepositoryScanner repoScanner;
 	
 	@Autowired
-	IssueGenerator issueGenerator;
+	GitConfig config;
 	
 	final static Logger LOGGER = Logger.getLogger(FinderInializer.class);
 
 	public void run(){
-		GitHubClient client = new GitHubClient();
-		client.setCredentials(username, password);
-		RepositoryService service = new RepositoryService(client);
 		List<Repository> repos = new ArrayList<Repository>();
 		try {
-			repos = service.getRepositories("davemcfadden");
+			//just grabbing a single repo for testing
+			repos.add(config.getService().getRepository("davemcfadden", "aws-dynamo-service"));
 		} catch (IOException e) {
 			LOGGER.error("Failed to retrieve repos : " + e);
 		}		
-		scanRepository(client, repos);
-	}
-
-	private void scanRepository(GitHubClient client, List<Repository> repos) {
-		for (Repository repo : repos ){
-			System.out.println(repo.getName());
-			if(repo.getName().equals("github-credential-finder")){
-				issueGenerator.createIssue(repo,client);
-			}
-		}
+		repoScanner.scanRepository(repos);
 	}
 }
 	
---
+++ src/main/java/org/credential/finder/config/GitConfig.java
@@ -0,0 +1,38 @@
+package org.credential.finder.config;
+
+import org.eclipse.egit.github.core.client.GitHubClient;
+import org.eclipse.egit.github.core.service.RepositoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class GitConfig {
+	
+	private GitHubClient client;
+	private RepositoryService service;
+	
+	@Autowired
+	public GitConfig(@Value("${github.username}") String username, @Value("${github.password}")	String password){
+		client = new GitHubClient();
+		this.client.setCredentials(username, password);
+		this.service = new RepositoryService(client);
+	}
+
+	public RepositoryService getService() {
+		return service;
+	}
+
+	public void setService(RepositoryService service) {
+		this.service = service;
+	}
+
+	public GitHubClient getClient() {
+		return client;
+	}
+
+	public void setClient(GitHubClient client) {
+		this.client = client;
+	}
+
+}
