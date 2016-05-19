f96a333553c122218475767f12d7defbc0ef2d83
davemcfadden <david.mcfadden@outlook.com>
2016-05-19 21:45:23 +0100

    Adding issue creating API

--- src/main/java/org/credential/finder/api/FinderInializer.java
+++ src/main/java/org/credential/finder/api/FinderInializer.java
@@ -1,10 +1,15 @@
 package org.credential.finder.api;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
+import org.apache.log4j.Logger;
+import org.credential.finder.issue.generator.IssueGenerator;
 import org.eclipse.egit.github.core.Repository;
 import org.eclipse.egit.github.core.client.GitHubClient;
 import org.eclipse.egit.github.core.service.RepositoryService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
@@ -14,25 +19,34 @@ public class FinderInializer{
 	@Value("${github.username}")
 	String username;
 
-
 	@Value("${github.password}")
 	String password;
+	
+	@Autowired
+	IssueGenerator issueGenerator;
+	
+	final static Logger LOGGER = Logger.getLogger(FinderInializer.class);
 
 	public void run(){
-
-		//OAuth2 token authentication
 		GitHubClient client = new GitHubClient();
 		client.setCredentials(username, password);
-		
 		RepositoryService service = new RepositoryService(client);
+		List<Repository> repos = new ArrayList<Repository>();
 		try {
-			for (Repository repo : service.getRepositories("defunkt"))
-			  System.out.println(repo.getName() + " Watchers: " + repo.getWatchers());
+			repos = service.getRepositories("davemcfadden");
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
+			LOGGER.error("Failed to retrieve repos : " + e);
+		}		
+		scanRepository(client, repos);
 	}
 
+	private void scanRepository(GitHubClient client, List<Repository> repos) {
+		for (Repository repo : repos ){
+			System.out.println(repo.getName());
+			if(repo.getName().equals("github-credential-finder")){
+				issueGenerator.createIssue(repo,client);
+			}
+		}
+	}
 }
+	
---
+++ src/main/java/org/credential/finder/issue/generator/IssueGenerator.java
@@ -0,0 +1,36 @@
+package org.credential.finder.issue.generator;
+
+import java.io.IOException;
+import java.util.Date;
+
+import org.apache.log4j.Logger;
+import org.eclipse.egit.github.core.Issue;
+import org.eclipse.egit.github.core.Repository;
+import org.eclipse.egit.github.core.client.GitHubClient;
+import org.eclipse.egit.github.core.service.IssueService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class IssueGenerator {
+	
+	final static Logger LOGGER = Logger.getLogger(IssueGenerator.class);
+	
+	
+	public Issue createIssue(Repository repo, GitHubClient client){
+		IssueService issueService = new IssueService(client);
+		Issue issue = new Issue();
+		issue.setTitle("Non encrypted credentials");
+		issue.setAssignee(repo.getOwner());
+		issue.setBody("Test issue creation");
+		issue.setCreatedAt(new Date());
+
+		Issue response = null;
+		try {
+			response = issueService.createIssue(repo, issue);
+		} catch (IOException e) {
+			LOGGER.error("Failed to create issue : " + e);
+		}
+		return response;
+	}
+
+}
