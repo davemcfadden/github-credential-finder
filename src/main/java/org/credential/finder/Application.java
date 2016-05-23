90ccb2a0d6e912ddc2707ac040c87e1d08d7bc33
CBrowne <calebrjbrowne@gmail.com>
2016-05-23 16:59:18 +0100

    quick hack violation finder method

--- src/main/java/org/credential/finder/Application.java
+++ src/main/java/org/credential/finder/Application.java
@@ -1,5 +1,6 @@
 package org.credential.finder;
 
+import org.credential.finder.analyzer.FileAnalyzer;
 import org.credential.finder.api.FinderInializer;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -8,7 +9,13 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Application {
   public static void main(String[] args) {
     ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-     context.getBean(FinderInializer.class).run();
+    context.getBean(FinderInializer.class).run();
     ((ConfigurableApplicationContext) context).close();
+
+    /**
+     * Example call Dave
+     */
+    // secretKey
+    FileAnalyzer.findPotentialViolations("src/main/java/org/credential/finder/Application.java");
   }
 }
--- src/main/java/org/credential/finder/analyzer/FileAnalyzer.java
+++ src/main/java/org/credential/finder/analyzer/FileAnalyzer.java
@@ -1,32 +1,48 @@
 package org.credential.finder.analyzer;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Stream;
 
 import org.apache.log4j.Logger;
 import org.credential.finder.api.FinderInializer;
 import org.credential.finder.issue.generator.IssueGenerator;
 import org.credential.finder.util.Util;
+import org.crendential.finder.pojo.Violation;
 import org.eclipse.egit.github.core.Repository;
 import org.eclipse.egit.github.core.RepositoryContents;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
-public class FileAnalyzer{
+public class FileAnalyzer {
 
-  @Autowired
-  private Util util;
+  private final static Logger LOGGER = Logger.getLogger(FinderInializer.class);
 
   @Autowired
   private IssueGenerator issueGenerator;
-  
-  private final static Logger LOGGER = Logger.getLogger(FinderInializer.class);
+
+  private static final List<String> hazardStrings = Util.getHazardStrings();
+
 
   public void analyseFile(String file, Repository repo, RepositoryContents content) {
-    List<String> hazardStrings = util.getHazardStrings();
     // TODO lets fine the stings
     LOGGER.debug("Searching file : " + content.getName() + " for passwords");
-    //issueGenerator.createIssue(repo,content);
+    // issueGenerator.createIssue(repo,content);
+  }
+
+  public static List<Violation> findPotentialViolations(String file) {
+    List<Violation> violations = new ArrayList<Violation>();
+    List<String> lines = Util.streamFile(file);
+    for (String hazard : hazardStrings) {
+      for (String line : lines) {
+        if (line.contains(hazard)) {
+          violations.add(new Violation(file, lines.indexOf(line) + 1));
+        }
+      }
+    }
+    violations.forEach(v -> LOGGER.debug("Potential violations found\n" + v.toString()));
+    return violations;
   }
 
 }
