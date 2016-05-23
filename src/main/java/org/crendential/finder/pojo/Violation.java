c731077933ecfab769d428116f7c3d8334407c85
CBrowne <calebrjbrowne@gmail.com>
2016-05-23 16:45:27 +0100

    potential pojo to contain info on violations found

---
+++ src/main/java/org/crendential/finder/pojo/Violation.java
@@ -0,0 +1,36 @@
+package org.crendential.finder.pojo;
+
+public class Violation {
+
+  public Violation() {}
+  
+  public Violation(String fileUrl, int lineNumber) {
+    this.fileUrl = fileUrl;
+    this.lineNumber = lineNumber;
+  }
+  
+  private String fileUrl;
+  private int lineNumber;
+
+  public String getFileUrl() {
+    return fileUrl;
+  }
+
+  public void setFileUrl(String fileUrl) {
+    this.fileUrl = fileUrl;
+  }
+
+  public int getLineNumber() {
+    return lineNumber;
+  }
+
+  public void setLineNumber(int lineNumber) {
+    this.lineNumber = lineNumber;
+  }
+
+  @Override
+  public String toString() {
+    return "Violation [fileUrl=" + fileUrl + ", lineNumber=" + lineNumber + "]";
+  }
+
+}
