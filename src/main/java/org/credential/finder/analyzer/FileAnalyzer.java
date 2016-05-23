f17492d0f6992ec53aea4bc5b02067f014c458f3
0e683e2b42e61904487c390cd54435406ded16e5 <calebrjbrowne@gmail.com>
2016-05-23 19:48:33 +0100

    javadoc

--- src/main/java/org/credential/finder/analyzer/FileAnalyzer.java
+++ src/main/java/org/credential/finder/analyzer/FileAnalyzer.java
@@ -5,12 +5,8 @@ import java.util.List;
 
 import org.apache.log4j.Logger;
 import org.credential.finder.api.FinderInializer;
-import org.credential.finder.issue.generator.IssueGenerator;
 import org.credential.finder.pojo.Violation;
 import org.credential.finder.util.Util;
-import org.eclipse.egit.github.core.Repository;
-import org.eclipse.egit.github.core.RepositoryContents;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -18,30 +14,54 @@ public class FileAnalyzer {
 
   private final static Logger LOGGER = Logger.getLogger(FinderInializer.class);
 
-  @Autowired
-  private IssueGenerator issueGenerator;
-
   private static final List<String> hazardStrings = Util.getHazardStrings();
 
+  /*
+   * @Autowired private IssueGenerator issueGenerator;
+   * 
+   * public void analyseFile(String file, Repository repo, RepositoryContents content) {
+   * LOGGER.debug("Searching file : " + content.getName() + " for passwords"); }
+   */
 
-  public void analyseFile(String file, Repository repo, RepositoryContents content) {
-    // TODO lets fine the stings
-    LOGGER.debug("Searching file : " + content.getName() + " for passwords");
-    // issueGenerator.createIssue(repo,content);
+  /**
+   * Scan the file provided and identify strings that may contain private information. Evaluate the
+   * candidates and return a list of potential violations.
+   * 
+   * @param filePath
+   * @return list of violations
+   */
+  public static List<Violation> findIssues(String filePath) {
+    List<Violation> issues = findPotentialViolations(filePath);
+    issues = evaluateAndFilter(issues);
+    return issues;
   }
 
-  public static List<Violation> findPotentialViolations(String file) {
+  /**
+   * Retrieve a list of strings representing all lines of the file provided. Evaluate each line to
+   * see if it contains hazard terms listed in flaggers.txt. Return a list of Violation objects
+   * describing the potentially exposed parts of the file.
+   * 
+   * @param filePath
+   * @return list of violations
+   */
+  private static List<Violation> findPotentialViolations(String filePath) {
     List<Violation> violations = new ArrayList<Violation>();
-    List<String> lines = Util.streamFile(file);
+    List<String> lines = Util.streamFile(filePath);
     for (String hazard : hazardStrings) {
       for (String line : lines) {
         if (line.contains(hazard)) {
-          violations.add(new Violation(file, line, (lines.indexOf(line) + 1)));
+          violations.add(new Violation(filePath, line, (lines.indexOf(line) + 1)));
         }
       }
     }
     violations.forEach(v -> LOGGER.debug("Potential violations found\n" + v.toString()));
     return violations;
+  }
+  
+
+  private static List<Violation> evaluateAndFilter(List<Violation> issues) {
+    // TODO Auto-generated method stub
+    return null;
   }
 
 }
